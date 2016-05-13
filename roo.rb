6411781f3d929b45df51a14feba31100ed66b2fa
Sella Rafaeli <sella.rafaeli@gmail.com>
2016-05-14 00:26:26 +0300

    auto-message.

--- roo.rb
+++ roo.rb
@@ -1,23 +1,25 @@
 # https://github.com/roo-rb/roo
 require 'roo'
 
-xl = Roo::Spreadsheet.open('src.xlsx')
+def sheet_to_arr(sheet_name)
+  xl = Roo::Spreadsheet.open('src.xlsx')
+  sheet = xl.sheet(sheet_name)
+  headers = sheet.row(3)
+  headers_hash = {}
+  headers.each do |h| headers_hash[h] = h if h.is_a? String end
 
-sheet = xl.sheet(0)
-sheet = xl.sheet('Dim - Doors')
+  def actual_cell(hash)
+    (hash['Level']!= nil || hash['Top Constraint']!= nil) &&
+     hash['Level'] != 'Level'
+  end
 
-headers = sheet.row(3)
-headers_hash = {}
-headers.each do |h| headers_hash[h] = h if h.is_a? String end
+  arr = []
+  sheet.each(headers_hash) do |hash|
+    arr << hash if actual_cell(hash)
+  end
 
-def actual_cell(hash)
-  (hash['Level']!= nil || hash['Top Constraint']!= nil) &&
-   hash['Level'] != 'Level'
+  arr
 end
 
-arr = []
-sheet.each(headers_hash) do |hash|
-  arr << hash if actual_cell(hash)
-end
-
-arr[0]
+arr  = sheet_to_arr('Dim - Doors')
+arr2 = sheet_to_arr('Dim - MMD')
